81d3d363123be2a55509dedb53615b0c7d442612
gh1352900923 <bob@example.com>
2017-09-29 14:56:25 +0400

    Lesson 1,2,3

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -60,10 +60,10 @@ fun timeForHalfWay(t1: Double, v1: Double,
     val s2: Double = t2 * v2
     val s3: Double = t3 * v3
     val halfS: Double = (s1 + s2 + s3) / 2.0
-    if (s1 >= halfS) return halfS / v1
+    return if (s1 >= halfS) halfS / v1
     else
-        if (s1 + s2 >= halfS) return t1 + (halfS - s1) / v2
-        else return t1 + t2 + (halfS - s1 - s2) / v3
+        if (s1 + s2 >= halfS) t1 + (halfS - s1) / v2
+        else t1 + t2 + (halfS - s1 - s2) / v3
 }
 
 /**
