7386ca3209d8b62d61a3ba0786b4ea9b9597a82a
AlmirBur <31883981+AlmirBur@users.noreply.github.com>
2017-11-12 00:47:59 +0300

    lesson6

--- src/lesson6/task2/Chess.kt
+++ src/lesson6/task2/Chess.kt
@@ -1,6 +1,10 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson6.task2
 
+import lesson6.task1.Triangle
+
+val name = "abcdefgh"
+
 /**
  * Клетка шахматной доски. Шахматная доска квадратная и имеет 8 х 8 клеток.
  * Поэтому, обе координаты клетки (горизонталь row, вертикаль column) могут находиться в пределах от 1 до 8.
@@ -21,7 +25,15 @@ data class Square(val column: Int, val row: Int) {
      * В нотации, колонки обозначаются латинскими буквами от a до h, а ряды -- цифрами от 1 до 8.
      * Для клетки не в пределах доски вернуть пустую строку
      */
-    fun notation(): String = TODO()
+    fun notation(): String = if (inside()) "${name[column - 1]}$row" else ""
+
+    override fun equals(other: Any?) = other is Square && column == other.column && row == other.row
+
+    override fun hashCode(): Int {
+        var result = column
+        result = 31 * result + row
+        return result
+    }
 }
 
 /**
@@ -31,7 +43,10 @@ data class Square(val column: Int, val row: Int) {
  * В нотации, колонки обозначаются латинскими буквами от a до h, а ряды -- цифрами от 1 до 8.
  * Если нотация некорректна, бросить IllegalArgumentException
  */
-fun square(notation: String): Square = TODO()
+fun square(notation: String): Square =
+        if (notation.length == 2 && notation[0] in name && notation[1].toString().toInt() in 1..8)
+            Square(name.indexOf(notation[0]) + 1, notation[1].toString().toInt())
+        else throw IllegalArgumentException()
 
 /**
  * Простая
@@ -56,7 +71,12 @@ fun square(notation: String): Square = TODO()
  * Пример: rookMoveNumber(Square(3, 1), Square(6, 3)) = 2
  * Ладья может пройти через клетку (3, 3) или через клетку (6, 1) к клетке (6, 3).
  */
-fun rookMoveNumber(start: Square, end: Square): Int = TODO()
+fun rookMoveNumber(start: Square, end: Square): Int = when {
+    !start.inside() || !end.inside() -> throw IllegalArgumentException()
+    start == end -> 0
+    start.column == end.column || start.row == end.row -> 1
+    else -> 2
+}
 
 /**
  * Средняя
